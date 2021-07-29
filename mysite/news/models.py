ef26a5e3eac927e49e1b0aa0811def21a2e044b7
Ririgirl <shatteredglass1@mail.ru>
2021-07-29 23:26:17 +0300

    third commit

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,9 +2,30 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=200, verbose_name='Наименование')
+    content = models.TextField(verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    create = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')  # защищена от удалени, если удалена категория
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import News
 
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello world!')
+    news = News.objects.all()
+    #print(timezone.localtime(timezone.now()))
+    return render(request, 'news/index.html', {'news':news, 'title':'Список новостей'})
 
-
-def test(request):
-    print(request)
-    return HttpResponse('<h1>Hi</h1>')
