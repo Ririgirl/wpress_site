25a0685eec914e0b8263a04d6f578e3714e1f7fd
Ririgirl <shatteredglass1@mail.ru>
2021-08-01 22:05:25 +0300

    6 commit

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -18,6 +19,8 @@ class News(models.Model):
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={'news_id': self.pk})
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
@@ -29,3 +32,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'create_id': self.pk})
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import News, Category
@@ -18,5 +18,11 @@ def get_category(request, create_id):
     news = News.objects.filter(create_id=create_id)
     category = Category.objects.get(pk=create_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+def view_news(request, news_id):
+    #news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {'news_item': news_item})
 
 
