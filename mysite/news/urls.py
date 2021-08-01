a94ff0a067abeccf821a1327534bf1a09cb25a53
Ririgirl <shatteredglass1@mail.ru>
2021-08-01 14:15:22 +0300

    5 commit

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('category/<int:create_id>/', get_category, name='category')
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    context = {
+        'news' : news,
+        'title': 'Список новостей'
+    }
     #print(timezone.localtime(timezone.now()))
-    return render(request, 'news/index.html', {'news':news, 'title':'Список новостей'})
+    return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, create_id):
+    news = News.objects.filter(create_id=create_id)
+    category = Category.objects.get(pk=create_id)
+    return render(request, 'news/category.html', {'news': news, 'category': category})
+
 
